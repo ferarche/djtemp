da25d17c0148e361715d7982ef717be5b7373375
ferachi <ferachi@yahoo.com>
2020-04-13 20:23:22 +0100

    Create template for the Vuejs framework
    Creates the template for the vuejs framework to
    be used alongside the django framework.
    The vuejs naturally to be the client side and django
    responsible for server side processing.
    A simple route and store for vuejs has
    also been configured for this template.

--- djvue/client/urls.py
+++ djvue/client/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from .views import AppView
 
 
 urlpatterns = [
-    path('', AppView.as_view(), name='index'),
+    re_path(r'^.*/$',AppView.as_view(),name='any_other')
 ]
